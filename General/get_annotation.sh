06b51b24a19c87b4d53f2bbe64d37b7c18ab3c71
Fabio Cumbo <fabio.cumbo@gmail.com>
2024-04-01 12:17:23 -0400

    Refactoring repo

--- General/get_annotation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -qO- https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_43/gencode.v43.annotation.gtf.gz \
-    | gunzip -c - \
-    | gtf2bed - \
-    | grep -w gene - \
-    > gencode.v43.genes.bed
